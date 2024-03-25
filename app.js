1b2b196459f5e63ade8318c8ae7be7cc88f68430
Wwright91 <wisdomwright-martin@pursuit.org>
2024-03-25 17:39:52 -0400

    Healthcheck Route Successful

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ data: "Server is running!" });
+});
+
+module.exports = app;
